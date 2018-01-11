8e112a142421664ff741891c2a4c7087560841a5
hdavtian <harmadavtian@gmail.com>
2018-01-11 14:16:21 -0800

    stylized posts, first image not showing up for some reason

--- src/components/posts/post/index.js
+++ src/components/posts/post/index.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import './post.css';
 
 export default class Post extends Component {
   render(){
     return(
-      <div>
-        <h3>{this.props.data.handle}</h3>
+      <div className="post">
+        <div className="photo" style={{'background-image' : 'url(' + this.props.data.photo + ')'}}></div>
+        <h5>{this.props.data.handle}</h5>
       </div>
     )
   }
