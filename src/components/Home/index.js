308036c89ed98e9d1fe95f08f0a64c8b460b1a47
hdavtian <harmadavtian@gmail.com>
2018-01-11 10:52:40 -0800

    first commit

---
+++ src/components/Home/index.js
@@ -0,0 +1,14 @@
+// basic react Component
+import React from 'react';
+
+export default class Home extends React.Component {
+
+  render(){
+    return(
+      <div>
+        <h1>Hello World!</h1>
+      </div>
+    )
+  }
+
+}
