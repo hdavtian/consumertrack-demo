859deffb475be178f390c12063d6a444c7f2dd2b
hdavtian <harmadavtian@gmail.com>
2018-01-11 11:31:36 -0800

    added jquery to support bootstrap, but it's not working, moving on to rest of app, will fix this later

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './includes/jquery';
+// window.jQuery = window.$ = $;
+import '../node_modules/jquery/dist/jquery.min.js';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 
 // my components
 import Home from './components/Home';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-
+        <Header />
         <Home />
+        <Footer />
 
       </div>
     );
