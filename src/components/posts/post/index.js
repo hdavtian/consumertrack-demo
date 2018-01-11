49b9957a235f87e616e878bc2dbc6cbae09d09c5
hdavtian <harmadavtian@gmail.com>
2018-01-11 14:19:39 -0800

    minor console error, fixed

--- src/components/posts/post/index.js
+++ src/components/posts/post/index.js
@@ -5,7 +5,7 @@ export default class Post extends Component {
   render(){
     return(
       <div className="post">
-        <div className="photo" style={{'background-image' : 'url(' + this.props.data.photo + ')'}}></div>
+        <div className="photo" style={{'backgroundImage' : 'url(' + this.props.data.photo + ')'}}></div>
         <h5>{this.props.data.handle}</h5>
       </div>
     )
