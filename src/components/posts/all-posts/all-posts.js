e35f72aff97668e3cea0e230953f26cbe88a5705
hdavtian <harmadavtian@gmail.com>
2018-01-11 16:06:09 -0800

    all posts, a bit hacky

--- src/components/posts/all-posts/all-posts.js
+++ src/components/posts/all-posts/all-posts.js
@@ -8,16 +8,47 @@ export default class AllPosts extends Component {
     super(props);
     this.state = {
       posts: []
-    }
+    };
+
+    // binding this
+    // this.componentDidMount = this.componentDidMount.bind(this);
   }
 
+  getUsersTotal(){
+    return 5
+  }
+
+  componentDidMount() {
+    // need to bind this better
+    var _this = this;
+    axios.all([
+      axios.get('https://dev-selfiegram.consumertrack.com/users/1'),
+      axios.get('https://dev-selfiegram.consumertrack.com/users/2'),
+      axios.get('https://dev-selfiegram.consumertrack.com/users/3'),
+      axios.get('https://dev-selfiegram.consumertrack.com/users/4')
+    ])
+      .then(
+        axios.spread(function (user1, user2, user3, user4){
+
+          let _allPosts = user1.data.posts
+            .concat(user2.data.posts)
+            .concat(user3.data.posts)
+            .concat(user4.data.posts);
+
+          // console.log('allInfo:', _allPosts);
+          _this.setState({posts: _allPosts});
+        }))
+      .catch(err => console.log(err))
+  }
+
+/*
   componentDidMount(){
     axios
       .get('https://dev-selfiegram.consumertrack.com/users/1')
       .then(res => this.setState({posts: res.data.posts}))
       .catch(err => console.log(err))
   }
-
+*/
   render(){
 
     const _posts = this.state.posts.map(function(post){
